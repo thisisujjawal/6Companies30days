aa02c5c68ffb1202dd6a1fca4d774486c5fcffab
Ujjawal Singhal <67731656+thisisujjawal@users.noreply.github.com>
2022-01-01 14:37:52 +0530

    Create ans 2.java

---
+++ company - 1 : Goldman Sachs/ans 2.java
@@ -0,0 +1,9 @@
+class Solution {
+    int doOverlap(int L1[], int R1[], int L2[], int R2[]) {
+        if(L1[0] > R2[0])return 0;
+        if(L2[0] > R1[0])return 0;
+        if(L1[1] < R2[1])return 0;
+        if(R1[1] > L2[1])return 0;
+        return 1;
+    }
+};
