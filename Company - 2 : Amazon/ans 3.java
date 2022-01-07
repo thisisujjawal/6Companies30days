83c938cdfe9f169ef4b268f927c69b5fa2801c1f
Ujjawal Singhal <67731656+thisisujjawal@users.noreply.github.com>
2022-01-07 12:00:26 +0530

    Create ans 3.java

---
+++ Company - 2 : Amazon/ans 3.java
@@ -0,0 +1,15 @@
+class Solution {
+    static ArrayList<Integer> max_of_subarrays(int arr[], int n, int k) {
+        Deque<Integer> dq = new LinkedList<>();
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(int i = 0 ; i < n ; i++){
+            while(!dq.isEmpty() && arr[i] > arr[dq.peekLast()])dq.pollLast();
+            dq.offerLast(i);
+            if(i >= k-1){
+                ans.add(arr[dq.peekFirst()]);
+                while(!dq.isEmpty() && dq.peekFirst() <= (i-k)+1)dq.pollFirst();
+            }
+        }
+        return ans;
+    }
+}
