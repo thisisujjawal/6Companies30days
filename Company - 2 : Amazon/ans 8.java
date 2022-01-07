64799959aa429181d0280c6169ebcfdf9c96779d
Ujjawal Singhal <67731656+thisisujjawal@users.noreply.github.com>
2022-01-07 22:22:34 +0530

    Create ans 8.java

---
+++ Company - 2 : Amazon/ans 8.java
@@ -0,0 +1,9 @@
+class Solution
+{
+    //Function to count number of ways to reach the nth stair 
+    //when order does not matter.
+    Long countWays(int m)
+    {
+        return (m/2) + 1l;
+    }    
+}
